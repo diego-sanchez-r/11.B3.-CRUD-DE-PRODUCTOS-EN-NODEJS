1cd9c9bf8e431fe1716690e4744aafa0d746daf3
diego-sanchez-r <diegosanchezrojo@gmail.com>
2022-02-12 20:46:58 +0100

    Ejercicio NodeJs

---
+++ modelo/equipo.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const EquiposSchema = mongoose.Schema(
+{
+    nombre: {
+        type:String,
+        required: [true,'El nombre es obligatorio']
+    },
+    ciudad: {
+        type:String,
+        required: [true,'La ciudad es obligatoria']
+    },
+    goles: {
+        type:Number,
+        required: [true,'Los goles son obligatorios']
+    },
+    imagen: {
+        type:String
+    },
+}
+)
+//sobreescribimos un método del Schema para modificar el objeto que exporta
+EquiposSchema.methods.toJSON = function() {
+    const { _id,...equipo} = this.toObject() ;
+    equipo.id=_id;
+    return equipo;
+}
+
+let Equipo = mongoose.model('Equipo',EquiposSchema);
+module.exports = Equipo;
