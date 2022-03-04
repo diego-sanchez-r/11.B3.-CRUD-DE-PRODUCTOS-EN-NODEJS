b0a7165952c84646783cc7a5e3148633056ad66e
diego-sanchez-r <diegosanchezrojo@gmail.com>
2022-03-04 20:36:38 +0100

    Crud Productos NodeJS

--- modelo/server.js
+++ modelo/server.js
@@ -7,6 +7,11 @@ const Rol = require('./rol');
 const bcryptjs = require('bcryptjs');
 const { body,validationResult,check} = require('express-validator');
 const port = process.env.PORT;
+const { generarJWT } = require("../helpers/generarJWT");
+const { validarJWT } = require("../middleware/validar-JWT");
+const {OAuth2Client} = require('google-auth-library');
+const fileUpload = require('express-fileupload');
+const { v4: uuidv4 } = require('uuid'); 
 class Server {
     constructor(){
         this.app = express();
@@ -14,15 +19,173 @@ class Server {
         this.middlewares();
         this.rutas();
     }
-    middlewares(){
-        this.app.use(express.json())//Middleware para leer json;
-        this.app.use(express.static('public'));
+    middlewares() {
+        this.app.use(express.json()); //Middleware para leer json;
+        this.app.use(express.static("public"));
         //^Middleware para servir la carpeta public
-    }
+        this.app.use(fileUpload({
+          useTempFiles: true,
+          tempFileDir: '/tmp/'
+        }));
+      }
     async conectarDB(){
         await dbConnection()
     }
     rutas(){
+         /**** RUTA SUBIR ARCHIVOS ****/
+    this.app.post(
+        "/subir",
+        
+        async function (req, res) {
+          if(!req.files){
+            res.status(400).json({
+              msg:'No se han mandado archivos',
+            });
+          }
+  
+          /* Esperamos un archivo con el nombre de un archivo */
+          if(!req.files.archivo){
+            res.status(400).json({
+              msg:'No se han mandado archivos',
+            });
+          }else{
+            const archivo = req.files.archivo;
+            const nombreCortado = archivo.name.split(".");
+            const extension = nombreCortado[nombreCortado.length -1];
+            //Validar la extension
+            const extensionesValidar = ['jpg','png','jpeg','gif'];
+            if(!extensionesValidar.includes( extension )){
+              return res.status(400).json({
+                msg: `La extension ${extension} no está permitida, ${extensionesValidar}`,
+              });
+            }
+            //Cambia el nombre 
+            const nombreTemp = uuidv4() + "." + extension;
+            const path = require('path');
+            const uploadPath = path.join(__dirname,'../archivos/',nombreTemp);
+            archivo.mv(uploadPath,function(err){
+              if(err){
+                return res.status(500).json(err);
+              }
+              res.status(200).json({
+                msg:'Archivo subido con exito',
+                uploadPath,
+                extension,
+                archivo
+              });
+            });
+          }
+        });
+        /******* RUTAS DE google *****/
+        this.app.post(
+            "/google",
+            check("id_token", "El token es necesario").not().isEmpty(),
+            async function (req, res) {
+              const erroresVal = validationResult(req);
+              //comprueba si ha habido errores en los checks
+              if (!erroresVal.isEmpty()) {
+                return res.status(400).json({ msg: erroresVal.array() });
+              }
+  
+  
+              try {
+                const { id_token } = req.body;
+                const { OAuth2Client } = require("google-auth-library");
+                const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
+  
+                const ticket = await client.verifyIdToken({
+                  idToken: id_token,
+                  audience: process.env.GOOGLE_CLIENT_ID, // Specify the CLIENT_ID of the app that accesses the backend
+                  // Or, if multiple clients access the backend:
+                  //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
+                });
+                const payload = ticket.getPayload();
+                console.log('PAYLOAD', payload);
+                const correo = payload.email;
+                const img = payload.picture;
+                const nombre = payload.name;
+                let miusuario = await Usuario.findOne({ correo });
+                if (!miusuario) {
+                  let data = {
+                    nombre, correo, password: "123", img, google: true,
+                    rol: 'USER_ROLE'
+                  }
+                  miusuario = new Usuario(data);
+                  await miusuario.save();
+                  console.log('Usuario a crear', miusuario);
+                }
+                // If request specified a G Suite domain:
+                // const domain = payload['hd'];
+  
+                /**** GENERO UN TOKEN VALIDO ****/
+                const tokenGenerado = await generarJWT(miusuario.id);
+                const id = miusuario.id;
+  
+                //******** ENVÍO UN RESPUESTA */
+                res.json({
+                  msg: "Todo bien con Google",
+                  id_token,
+                  token: tokenGenerado,
+                  miusuario
+                });
+              } catch (error) {
+                res.json({
+                  msg: "ERROR DE VERIFICACION DE GMAIL",
+                  id_token
+                });
+              }
+            }
+          );
+  
+        /******* RUTAS DEL LOGIN *****/
+        this.app.post(
+            "/login",
+            check("correo", "El correo no es válido").isEmail(),
+            check("password", "La contraseña no puede ser vacía").not().isEmpty(),
+            async function (req, res) {
+              const erroresVal = validationResult(req);
+              //comprueba si ha habido errores en los checks
+              if (!erroresVal.isEmpty()) {
+                return res.status(400).json({ msg: erroresVal.array() });
+              }
+              const { correo, password } = req.body;
+              try {
+                //verifico si el correo existe en la BD
+                const miusuario = await Usuario.findOne({ correo });
+                if (!miusuario) {
+                  res.status(400).json({
+                    msg: "El correo no existe",
+                    correo,
+                  });
+                } else {
+                  //verifico la contraseña
+                  const validPassword = bcryptjs.compareSync(
+                    password,
+                    miusuario.password
+                  );
+                  if (!validPassword) {
+                    res.status(400).json({
+                      msg: "El password no es correcto",
+                    });
+                  } else {
+                    //genero el JWT
+                    const token = await generarJWT(miusuario.id);
+                    const id = miusuario.id;
+                    res.json({
+                      msg: "Login OK",
+                      token,
+                      id,
+                    });
+                  }
+                }
+              } catch (error) {
+                console.log(error);
+                res.status(500).json({
+                  msg: "Error de autenticación",
+                });
+              }
+            }
+          );
     /******* RUTAS DEL EQUIPO *****/ 
     //Get muestra el equipo y hay que pasarle la id.
     this.app.get('/webresources/generic/equipos/:id', async function (req, res) {
@@ -94,16 +257,7 @@ class Server {
             body('correo').isEmail(),
             check('nombre','El nombre es obligatorio').not().isEmpty(),
             check('password','El password debe tener al menos 6 caracteres').isLength({min:6}),
-            //check('rol','El rol no es válido').isIn(['ADMIN_ROLE','USER_ROLE']),
-            check('rol').custom(
-                    async function(rol) {
-                        const existeRol = await Rol.findOne({rol});
-                        if (!existeRol ) {
-                            throw new Error(`El rol ${rol} no está en la BD`)
-                        }
-                    }
-
-            ),
+            check('rol','El rol no es válido').isIn(['ADMIN_ROLE','USER_ROLE']),
             check('correo').custom(
                 async function(correo) {
                     const existeCorreo = await Usuario.findOne({correo});
@@ -148,7 +302,7 @@ class Server {
                 resto
             })
           })
-    this.app.delete('/api/:id', async function (req, res) {
+    this.app.delete('/api/:id',validarJWT, async function (req, res) {
             const id = req.params.id;
             await Usuario.findByIdAndDelete(id);
             res.status(403).json({
